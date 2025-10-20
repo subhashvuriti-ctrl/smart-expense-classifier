98bc44da894c1710eae33965dcb88f1cd3b1186c
subhashvuriti-ctrl <subhashvuriti@gmail.com>
2025-10-20 11:21:34 +0530

    Create schema.py

---
+++ src/schema.py
@@ -0,0 +1,8 @@
+COLUMNS = {
+    "date": "date",
+    "amount": "amount",
+    "description": "description",
+    "category": "category",  # only for training data
+}
+
+MANDATORY_FOR_APP = ["date", "amount", "description"]
